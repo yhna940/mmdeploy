f1286fb69b6e9ad8b34d966c670594205d6758d5
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2023-07-26 17:10:47 +0900

    Fix script

--- mmdeploy/mmcv/ops/nms_rotated.py
+++ mmdeploy/mmcv/ops/nms_rotated.py
@@ -3,8 +3,49 @@ import torch
 from torch import Tensor
 
 
+@torch.jit.ignore
+def ext_nms_rotate(bbox: Tensor, scores: Tensor, order: Tensor,
+                   dets_sorted: Tensor, iou_threshold: float,
+                   multi_label: int):
+    from mmcv.utils import ext_loader
+    ext_module = ext_loader.load_ext('_ext', ['nms_rotated'])
+    return ext_module.nms_rotated(bbox, scores, order, dets_sorted,
+                                  iou_threshold, multi_label)
+
+
 class ONNXNMSRotatedOp(torch.autograd.Function):
     """Create onnx::NMSRotated op."""
+
+    @staticmethod
+    @torch.jit.script
+    def script_forward(boxes: Tensor, scores: Tensor, iou_threshold: float,
+                       score_threshold: float) -> Tensor:
+        batch_size, num_class, _ = scores.shape
+
+        indices = []
+        for batch_id in range(batch_size):
+            for cls_id in range(num_class):
+                _boxes = boxes[batch_id, ...]
+                # score_threshold=0 requires scores to be contiguous
+                _scores = scores[batch_id, cls_id, ...].contiguous()
+                valid_mask = _scores > score_threshold
+                _boxes, _scores = _boxes[valid_mask], _scores[valid_mask]
+                if _boxes.shape[0] == 0:
+                    continue
+                valid_inds = torch.nonzero(valid_mask).squeeze(dim=1)
+                _, order = _scores.sort(0, descending=True)
+                dets_sorted = _boxes.index_select(0, order)
+                box_inds = ext_nms_rotate(_boxes, _scores, order, dets_sorted,
+                                          iou_threshold, 0)
+                box_inds = valid_inds[box_inds]
+                batch_inds = torch.zeros_like(box_inds) + batch_id
+                cls_inds = torch.zeros_like(box_inds) + cls_id
+                indices.append(
+                    torch.stack([batch_inds, cls_inds, box_inds], dim=-1))
+
+        indices = torch.cat(indices) if len(indices) > 0 else torch.zeros(
+            (0, 3), dtype=torch.long, device=boxes.device)
+        return indices
 
     @staticmethod
     def forward(ctx, boxes: Tensor, scores: Tensor, iou_threshold: float,
@@ -23,38 +64,9 @@ class ONNXNMSRotatedOp(torch.autograd.Function):
         Returns:
             Tensor: Selected indices of boxes.
         """
-        from mmcv.utils import ext_loader
-        ext_module = ext_loader.load_ext('_ext', ['nms_rotated'])
-        batch_size, num_class, _ = scores.shape
+        return ONNXNMSRotatedOp.script_forward(boxes, scores, iou_threshold,
+                                               score_threshold)
 
-        indices = []
-        for batch_id in range(batch_size):
-            for cls_id in range(num_class):
-                _boxes = boxes[batch_id, ...]
-                # score_threshold=0 requires scores to be contiguous
-                _scores = scores[batch_id, cls_id, ...].contiguous()
-                valid_mask = _scores > score_threshold
-                _boxes, _scores = _boxes[valid_mask], _scores[valid_mask]
-                if _boxes.shape[0] == 0:
-                    continue
-                valid_inds = torch.nonzero(
-                    valid_mask, as_tuple=False).squeeze(dim=1)
-                _, order = _scores.sort(0, descending=True)
-                dets_sorted = _boxes.index_select(0, order)
-                box_inds = ext_module.nms_rotated(_boxes, _scores, order,
-                                                  dets_sorted, iou_threshold,
-                                                  0)
-                box_inds = valid_inds[box_inds]
-                batch_inds = torch.zeros_like(box_inds) + batch_id
-                cls_inds = torch.zeros_like(box_inds) + cls_id
-                indices.append(
-                    torch.stack([batch_inds, cls_inds, box_inds], dim=-1))
-
-        indices = torch.cat(indices) if indices else torch.empty(
-            (0, 3), dtype=torch.long, device=boxes.device)
-        return indices
-
-    @staticmethod
     def symbolic(g, boxes: Tensor, scores: Tensor, iou_threshold: float,
                  score_threshold: float):
         """Symbolic function for onnx::NMSRotated.
